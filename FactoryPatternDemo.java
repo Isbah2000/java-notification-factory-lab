a1cf0e64b371e2ce6fa3e92f0e88c54860cfa07a
Isbah2000 <k258232@nu.edu.pk>
2025-11-29 17:45:05 +0500

    Create FactoryPatternDemo.java

---
+++ FactoryPatternDemo.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.List;
+public class FactoryPatternDemo {
+public static void main(String[] args) {
+NotificationFactory factory = new NotificationFactory();
+// Simulated notification requests: type, recipient, message
+List<String[]> requests = Arrays.asList(
+new String[] {"email", "alice@example.com", "Welcome to the course!"},
+new String[] {"SMS", "+923001234567", "Class starts at 10:00 AM"},
+new String[] {"Push", "device-abc-123", "Assignment due tomorrow"},
+new String[] {"fax", "1234", "This will show unknown"}
+);
+for (String[] req : requests) {
+String type = req[0];
+String recipient = req[1];
+String message = req[2];
+Notification notifier = factory.getNotification(type);
+System.out.print("Request: " + type + " -> ");
+if (notifier != null) {
+notifier.send(recipient, message);
+} else {
+System.out.println("No such notification channel: " + type);
+}
+}
+}
+}
